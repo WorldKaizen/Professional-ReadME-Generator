bd3c18786e81ebfc7b9d986db15052ddae41ff31
WorldKaizen <73084934+WorldKaizen@users.noreply.github.com>
2021-03-28 15:11:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,89 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//Prompt the user questions to populate the README.md
+const   questions = [
+    
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    
+    
+];
+//write function to write file
+function writeToFile(fileName,data) {
+    fs.writeFile(fileName,data,err => {
+        if(err){
+            throw err;
+        }
+    });
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to initialize program function to initialize program
+function init() {
+    prompt(questions).then(answers => {
 
-// Function call to initialize app
-init();
-           
-      
+        const response = generateMarkdown(answers);
+        console.log
+    };
+   
